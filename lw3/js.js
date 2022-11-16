ef65091564c4d86ebf1d1cf3088b64ef1630f862
pozitifon <114288887+pozitifon@users.noreply.github.com>
2022-11-16 20:11:33 +0300

    сделал 3labb

---
+++ lw3/js.js
@@ -0,0 +1,43 @@
+const btn = document.querySelector('#btn');
+const main = document.querySelector('#main');
+const input = document.querySelector('#input');
+
+
+btn.addEventListener('click', (e) => {
+    if (input.value === '') return
+    createdelete(input.value)
+    input.value = ''
+})
+
+function createdelete(value) {
+    console.log(value)
+
+    const li = document.createElement('li');
+    const delet = document.createElement('button');
+    const ready = document.createElement('button');
+    const div = document.createElement('div');
+
+
+    li.className = 'li';
+    li.textContent = value;
+    delet.className = 'delet';
+    delet.innerHTML = '<img src="./icons/musor.svg">';
+
+    li.appendChild(delet);
+
+    ready.className = 'ready';
+    ready.innerHTML = '<img src="./icons/galochka.svg">';
+
+    li.appendChild(ready);
+
+    delet.addEventListener('click', (e) => {
+        main.removeChild(li)
+    })
+
+    ready.addEventListener('click', (e) => {
+        li.classList.toggle('create')
+    })
+
+    main.appendChild(li)
+    
+}
